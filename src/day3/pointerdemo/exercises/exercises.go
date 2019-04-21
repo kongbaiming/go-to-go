dcf95ecb91fea42455456a7b3ecff5f86097f0e4
kinglong <zhangjinlong@gitedit.com>
2019-04-21 20:11:59 +0800

    指针练习

--- src/day3/pointerdemo/exercises/exercises.go
+++ src/day3/pointerdemo/exercises/exercises.go
@@ -5,10 +5,17 @@ import "fmt"
 
 func main() {
 	var num int = 20
-	fmt.Printf("num的内存地址是:%v\n",&num)
+	fmt.Printf("num的内存地址是:%v\n", &num)
 	var ptr *int = &num
-	fmt.Printf("ptr指向的内存地址是:%v\n",ptr)
+	fmt.Printf("ptr指向的内存地址是:%v\n", ptr)
 	*ptr = 22 //修改num的值
-	fmt.Printf("num的值是:%v\n",num)
-}
+	fmt.Printf("num的值是:%v\n", num)
 
+	var a int = 300
+	var b int = 400
+	ptr = &a
+	*ptr = 100
+	ptr = &b
+	*ptr = 200
+	fmt.Printf("a=%d,b=%d,*ptr=%d", a, b, *ptr)
+}
