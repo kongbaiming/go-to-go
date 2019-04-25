a059d40b1e64de8e29dd562bef608fe3441765b3
kinglong <zhangjinlong@gitedit.com>
2019-04-25 22:52:24 +0800

    练习题

---
+++ src/day3/homework/homework.go
@@ -0,0 +1,26 @@
+// 1.  假如还有97天放假，问，还剩几个星期零几天
+// 2.  求华氏温度对应的摄氏温度
+
+/*
+摄氏温标（C）和华氏温标（F）之间的换算关系为：
+
+F=C×1.8+32
+
+C=(F-32)÷1.8
+*/
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var days int = 97
+	weeks := days / 7
+	day := days % 7
+	fmt.Printf("还剩%d个星期，%d天\n", weeks, day)
+
+	fahrenheit := 100
+	var celsiust float32 = float32(fahrenheit - 32) / 1.8
+	fmt.Printf("华氏度%v度换算成摄氏度后为:%v度\n",fahrenheit,celsiust)
+}
